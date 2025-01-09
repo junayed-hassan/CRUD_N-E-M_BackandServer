fc711e981fa11945bdbcc958dfa1165e0d5bc02f
junayed-hassan <junayedhassan102045@gmail.com>
2025-01-09 15:21:27 +0600

    up

--- index.js
+++ index.js
@@ -118,12 +118,58 @@ async function run() {
     });
 
     // POST route to add new users
-  app.post("/users", async ( req, res) => {
-    const user = req.body
-    const result = await usersCollection.insertOne(user);
-    res.send(result)
-    
-  })
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // GET route to retrieve all users
+    app.get("/users", async (req, res) => {
+      try {
+        const cursor = usersCollection.find({});
+        const users = await cursor.toArray();
+        res.status(200).send(users);
+      } catch (error) {
+        console.error("Error getting users:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
+
+    // DELETE route to delete a users by id
+    app.delete("/users/:id", async (req, res) => {
+      try {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await usersCollection.deleteOne(query);
+        res.send(result);
+      } catch (error) {
+        console.error("Error inserting users:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
+
+    // PUT route a users
+    app.patch("/users", async (req, res) => {
+      try {
+        const user = req.body;
+        const email = user.email;
+        const query = {email};
+        const updateObject = {
+          $set: {
+            lastLoginAt: user.lastLoginAt
+          }
+        };
+        const result = await usersCollection.updateOne(
+          query,
+          updateObject,
+        );
+        res.send(result);
+      } catch (error) {
+        console.error("Error inserting coffee:", error);
+        res.status(500).send({ message: "Internal Server Error" });
+      }
+    });
 
     // Additional Routes/Endpoints can be added here
   } catch (error) {
